80c30242fe49259fbefd15ae50b51d10a21a7fe0
Pierre-Luc S <PierreLucSimard@squarelab.ca>
2019-06-16 15:51:16 -0400

    fix check git status porcelaine

--- bash/vsts-git.sh
+++ bash/vsts-git.sh
@@ -16,16 +16,16 @@ function vstsCloneRepo() {
     local gitrepo_url="https://dev.azure.com/${VSTS_ORGANIZATION}/${VSTS_PROJECT}/_git/${VSTS_GIT_REPO}"
     
     echo "Cloning git repository ${gitrepo_url}"
-    git -C $GIT_LOCALREPO_PATH remote add origin $gitrepo_url &> /dev/null
-    git -C $GIT_LOCALREPO_PATH fetch origin &> /dev/null
-    git -C $GIT_LOCALREPO_PATH checkout master &> /dev/null
+    git -C $GIT_LOCALREPO_PATH remote add origin $gitrepo_url > /dev/null
+    git -C $GIT_LOCALREPO_PATH fetch origin > /dev/null
+    git -C $GIT_LOCALREPO_PATH checkout master > /dev/null
 }
 
 function vstsUploadChanges() {
    
-    if [[ $(git -C $GIT_LOCALREPO_PATH status --porcelain) ]]; then return 0; fi
+    if [[ -z $(git -C $GIT_LOCALREPO_PATH status --porcelain) ]]; then return 0; fi
 
     echo "Updating git"
-    git -C $GIT_LOCALREPO_PATH commit -am $1 &> /dev/null
-    git -C $GIT_LOCALREPO_PATH push &> /dev/null
+    git -C $GIT_LOCALREPO_PATH commit -am $1 > /dev/null
+    git -C $GIT_LOCALREPO_PATH push > /dev/null
 }
