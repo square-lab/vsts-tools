878d6cefdbcacaeb61dbdf29bbc0d0f1e364c583
Pierre-Luc Simard <pierrelucsimard@squarelab.ca>
2019-06-16 01:29:24 -0400

    Update vsts-git.sh

--- bash/vsts-git.sh
+++ bash/vsts-git.sh
@@ -1,5 +1,13 @@
+# env vars: Mandatory: GIT_LOCALREPO_PATH, VSTS_ORGANIZATION, VSTS_PROJECT, VSTS_GIT_REPO; Optionnal: VSTS_PAT
+
 function vstsCloneRepo() {
 
+    if [[ -n $VSTS_PAT ]]
+    then
+        local auth_token=$(echo -n ":${VSTS_PAT}" | openssl base64 | tr -d '\n')
+        git -C $GIT_LOCALREPO_PATH config http.extraheader "Authorization: Basic ${auth_token}"
+    fi  
+    
     if [[ -z $VSTS_ORGANIZATION || -z $VSTS_PROJECT || -z $VSTS_GIT_REPO ]]
     then
         echo "vsts: missing environment variables" >&2
@@ -9,8 +17,8 @@ function vstsCloneRepo() {
     
     echo "Cloning git repository ${gitrepo_url}"
     git -C $GIT_LOCALREPO_PATH remote add origin $gitrepo_url &> /dev/null
-    git -C $CLUSTERINFO_PATH fetch origin &> /dev/null
-    git -C $CLUSTERINFO_PATH checkout master &> /dev/null
+    git -C $GIT_LOCALREPO_PATH fetch origin &> /dev/null
+    git -C $GIT_LOCALREPO_PATH checkout master &> /dev/null
 }
 
 function vstsUploadChanges() {
