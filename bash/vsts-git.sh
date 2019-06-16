a52d2fdfca1b7648a905ce64b73fbde17ea00ca1
Pierre-Luc Simard <pierrelucsimard@squarelab.ca>
2019-06-16 01:23:42 -0400

    Update vsts-git.sh

--- bash/vsts-git.sh
+++ bash/vsts-git.sh
@@ -1,25 +1,3 @@
-function configureGitLocalRepo() {
-
-    if [[ -z $GIT_LOCALREPO_PATH ]]
-    then
-        echo "git: missing environment variables" >&2
-        return 1
-    fi
-
-    echo "Configuring git repository"
-    mkdir -p $GIT_LOCALREPO_PATH
-
-    git -C $GIT_LOCALREPO_PATH init &> /dev/null
-    git -C $GIT_LOCALREPO_PATH config user.email $GIT_USER_EMAIL
-    git -C $GIT_LOCALREPO_PATH config user.name $GIT_USER_NAME
-
-    if [[ -n $VSTS_PAT ]]
-    then
-        local auth_token=$(echo -n ":${VSTS_PAT}" | openssl base64 | tr -d '\n')
-        git -C $GIT_LOCALREPO_PATH config http.extraheader "Authorization: Basic ${auth_token}"
-    fi   
-}    
-
 function vstsCloneRepo() {
 
     if [[ -z $VSTS_ORGANIZATION || -z $VSTS_PROJECT || -z $VSTS_GIT_REPO ]]
